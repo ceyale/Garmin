550373affd60597b4cddedf4dca02bb6ad7e65e3
Etienne <etienne.layec@gmail.com>
2026-08-02 21:16:01 +0200

    debuggin

--- windsurf/gen_key.bat
+++ windsurf/gen_key.bat
@@ -1,2 +1,3 @@
 @echo off
-"C:\PROGRA~1\Git\usr\bin\openssl.exe" genrsa -out "C:\Users\franc\Documents\Etienne\Garmin\developer_key" 2048
+"C:\PROGRA~1\Git\usr\bin\openssl.exe" genrsa -out "C:\Users\franc\Documents\Etienne\Garmin\developer_key.pem" 2048
+"C:\PROGRA~1\Git\usr\bin\openssl.exe" rsa -in "C:\Users\franc\Documents\Etienne\Garmin\developer_key.pem" -outform DER -out "C:\Users\franc\Documents\Etienne\Garmin\developer_key"
