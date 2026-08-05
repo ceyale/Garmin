954c96366847bf2392687ee70685b0b6d1ef078d
ceyale <etienne.layec@gmail.com>
2026-08-05 14:32:44 +0200

    Aktualisieren von build.bat

--- windsurf/build.bat
+++ windsurf/build.bat
@@ -1,3 +1,3 @@
 @echo off
 set "JAVA_BIN=C "PATH=%JAVA_BIN%;%PATH%"
-call "C:\Users\franc\AppData\Roaming\Garmin\ConnectIQ\Sdks\connectiq-sdk-win-9.2.0-2026-06-09-92a1605b2\bin\monkeyc.bat" -d fr745 -y "c:\Users\franc\Documents\Etienne\Garmin\developer_key" -o windsurf.prg -f monkey.jungle %*
+call "C:/" -o windsurf.prg -f monkey.jungle %*
